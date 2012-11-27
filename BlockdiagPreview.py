0009dcd7c62816746742c19b770d555b9604c1b5
mstssk <mstssk+github@gmail.com>
2012-11-28 01:56:18 +0900

    bug fix for file path encoding on Windows

--- BlockdiagPreview.py
+++ BlockdiagPreview.py
@@ -4,8 +4,6 @@ import tempfile
 import subprocess
 import desktop
 
-# TODO getTempPreviewPath を何度も使うのが微妙 
-# TODO blockdiagコマンドは内蔵させる？
 
 def getTempPreviewPath(view):
 	" return a permanent full path of the temp preview file "
@@ -13,21 +11,33 @@ def getTempPreviewPath(view):
 	tmp_fullpath = os.path.join(tempfile.gettempdir(), tmp_filename)
 	return tmp_fullpath
 
+
+def encodeFilePath(path):
+	" encode file path for each platforms encoding "
+	if sublime.platform() == 'windows':
+		return path.encode('cp932')
+	return path.encode('utf_8')
+
+
 class BlockdiagPreviewListener(sublime_plugin.EventListener):
 	def on_post_save(self, view):
 		if view.file_name().endswith('.diag'):
 			temp_file = getTempPreviewPath(view)
 			if os.path.isfile(temp_file):
 				# reexec conversion
-				view.run_command('blockdiag_preview', {'path': temp_file})
+				view.run_command('blockdiag_preview', {'open': False})
 				sublime.status_message('Blockdiag preview file updated')
 
+
 class BlockdiagPreviewCommand(sublime_plugin.TextCommand):
-	def run(self, edit, path=''):
-		if len(path) == 0:
-			path = getTempPreviewPath(self.view)
-		retcode = subprocess.call(['blockdiag', self.view.file_name(), '-o', path])
+	def run(self, edit, open=True):
+		temp = encodeFilePath(getTempPreviewPath(self.view))
+		source = encodeFilePath(self.view.file_name())
+		retcode = subprocess.call(['blockdiag', source, '-o', temp])
 		if retcode < 0:
-			sublime.status_message('error!')
-		else:
-			desktop.open(path)
+			msg = 'Blockdiag process is failed!'
+			print msg
+			sublime.status_message(msg)
+		elif open:
+			desktop.open(temp)
+
