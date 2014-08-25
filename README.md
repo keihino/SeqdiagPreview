6a2977e72dcf9f02389d0b10b0af3a672807068c
Kei HINO <kei.hino+github@gmail.com>
2014-08-25 13:01:17 +0900

    changes for Seqdiag preview

--- README.md
+++ README.md
@@ -1,25 +1,28 @@
-# Sublime Text 2 Blockdiag preview
+# Sublime Text 2 Seqdiag preview
 
-**Caution!**: Currently this plugin needs **blockdiag** command which is installed your system.
+**Caution!**: Currently this plugin needs **seqdiag(w/ blockdiag)** command which is installed your system.
 
 ## About :
 
-*[blockdiag][0]*  generates diagram simply!<br>
+*[seqdiag][0]*  generates diagram simply!<br>
 you can preview generated diagram quickly with this plugin.
+
+**This plugin is forked from *[Blockdiag Preview][2]*. Thanks a lot.**
 
 ## Installation :
 
- - you should install [blockdiag][0] to your system.
+ - you should install [seqdiag][0] to your system.
  - you should use [sublime package manager][1]
  - use `ctrl+shift+P` then `Package Control: Add Repository`
- - add `https://github.com/mstssk/BlockdiagPreview.git`
+ - add `https://github.com/keihino/SeqdiagPreview.git`
  - use `ctrl+shift+P` again, then `Package Control: Install Package`
- - look for `BlockdiagPreview` and install it
+ - look for `SeqdiagPreview` and install it
 
 ## Usage :
 
- - save *foobar<strong>.diag</strong>* then use `ctrl+shift+P` then `BlockdiagPreview` to launch a preview
+ - save *foobar<strong>.diag</strong>* then use `ctrl+shift+P` then `SeqdiagPreview` to launch a preview
  - file name should be end with **.diag**.
 
  [0]: http://blockdiag.com/
  [1]: http://wbond.net/sublime_packages/package_control
+ [2]: https://github.com/mstssk/BlockdiagPreview
--- SeqdiagPreview.py
+++ SeqdiagPreview.py
@@ -4,12 +4,12 @@ import tempfile
 import subprocess
 import desktop
 
-settings = sublime.load_settings('BlockdiagPreview.sublime-settings')
+settings = sublime.load_settings('SeqdiagPreview.sublime-settings')
 
 
 def getTempPreviewPath(view):
 	" return a permanent full path of the temp preview file "
-	tmp_filename = 'blockdiag_preview_%s.png' % view.id()
+	tmp_filename = 'seqdiag_preview_%s.png' % view.id()
 	tmp_fullpath = os.path.join(tempfile.gettempdir(), tmp_filename)
 	return tmp_fullpath
 
@@ -21,23 +21,23 @@ def encodeFilePath(path):
 	return path.encode('utf_8')
 
 
-class BlockdiagPreviewListener(sublime_plugin.EventListener):
+class SeqdiagPreviewListener(sublime_plugin.EventListener):
 	def on_post_save(self, view):
 		if view.file_name().endswith('.diag'):
 			temp_file = getTempPreviewPath(view)
 			if os.path.isfile(temp_file):
 				# reexec conversion
-				view.run_command('blockdiag_preview', {'open': settings.get('open_on_save', False)})
-				sublime.status_message('Blockdiag preview file updated')
+				view.run_command('seqdiag_preview', {'open': settings.get('open_on_save', false)})
+				sublime.status_message('Seqdiag preview file updated')
 
 
-class BlockdiagPreviewCommand(sublime_plugin.TextCommand):
+class SeqdiagPreviewCommand(sublime_plugin.TextCommand):
 	def run(self, edit, open=True):
 		temp = encodeFilePath(getTempPreviewPath(self.view))
 		source = encodeFilePath(self.view.file_name())
-		retcode = subprocess.call(['blockdiag', source, '-o', temp])
+		retcode = subprocess.call(['seqdiag', source, '-o', temp])
 		if retcode < 0:
-			msg = 'Blockdiag process is failed!'
+			msg = 'Seqdiag process is failed!'
 			print msg
 			sublime.status_message(msg)
 		elif open:
